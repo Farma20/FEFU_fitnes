0a10c3c0b21f8985a36a17f633d20ee9d7de0d71
ÐFarma20 <kustov.ius@students.dvfu.ru>
2023-02-22 05:54:33 +1000

    Реализована навигационная панель

--- app/src/main/java/com/example/fefu_fitnes/UI/Views/MainActivity.kt
+++ app/src/main/java/com/example/fefu_fitnes/UI/Views/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.fefu_fitnes.UI.Views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.example.fefu_fitnes.R
+import com.example.fefu_fitnes.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //отключение темной темы
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
@@ -16,8 +23,31 @@ class MainActivity : AppCompatActivity() {
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         if(currentFragment == null){
-            val fragment = TimetableFragment.newInstance()
+            val fragment = MainMenuFragment.newInstance()
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
         }
+
+        binding.main.setBackgroundResource(R.drawable.activity_main_bottom_navigate_main_pick)
+
+        binding.main.setOnClickListener {
+            onFragmentSelected(MainMenuFragment.newInstance())
+            it.setBackgroundResource(R.drawable.activity_main_bottom_navigate_main_pick)
+            binding.tables.setBackgroundResource(R.drawable.activity_main_bottom_navigate_tables)
+        }
+
+        binding.tables.setOnClickListener {
+            onFragmentSelected(TimetableFragment.newInstance())
+            it.setBackgroundResource(R.drawable.activity_main_bottom_navigate_tables_pick)
+            binding.main.setBackgroundResource(R.drawable.activity_main_bottom_navigate_main)
+        }
+
+
+    }
+
+    fun onFragmentSelected(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
